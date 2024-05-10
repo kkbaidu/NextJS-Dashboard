59100ea5e7af695004592255f82273ed01156a43
kkbaidu <kingsleybaidu99@gmail.com>
2024-05-10 04:33:37 +0000

    adding search and Pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,13 +13,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 3000);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
